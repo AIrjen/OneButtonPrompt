a4ed24ff6278b6c522e7a4cc94c68f4ef55c926e
AIrjen <arjenkreuk@gmail.com>
2023-08-11 17:05:10 +0200

    Adding auto subject strenght

--- prompttester.py
+++ prompttester.py
@@ -46,4 +46,4 @@ def generateprompts(amount = 1,insanitylevel="5",subject="all", artist="all", im
     print("")
     print("All done!")
 
-generateprompts(10,5,"all","all","all",False,"","","PREFIXPROMPT" ,"SUFFIXPROMPT","",1,"","[hello there|cool man]",True,"",100, "all", "all", "all", "all", False)
+generateprompts(10,8,"all","all","all",False,"","","PREFIXPROMPT" ,"SUFFIXPROMPT","",1,"","(hello:1.3)",True,"",100, "all", "all", "all", "all", False)

37fa40f21522f784c5aecc31af9357d7659655bc
AIrjen <130234949+AIrjen@users.noreply.github.com>
2023-10-21 09:37:11 +0200

    minifix

--- prompttester.py
+++ prompttester.py
@@ -79,7 +79,7 @@ def generateprompts(amount = 1,insanitylevel="5",subject="all", artist="all", im
     print("")
     print("All done!")
 
-generateprompts(1000,5
+generateprompts(10000,5
                 ,"all" # subject
                 ,"all" # artists
                 ,"all" # image type  "only other types", "only templates mode", "art blaster mode", "quality vomit mode", "color cannon mode", "unique art mode", "massive madness mode", "photo fantasy mode", "subject only mode", "fixed styles mode"
